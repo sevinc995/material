bbe6fc6c4df3d6118cf00de3d9a9f8d800ed5fc8
sevinc995 <sevincseferova25@gmail.com>
2025-09-10 10:48:57 +0000

    change

--- src/App.js
+++ src/App.js
@@ -1,37 +1,18 @@
+
 import * as React from 'react';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import FolderIcon from '@mui/icons-material/Folder';
-import RestoreIcon from '@mui/icons-material/Restore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+import {Switch, Button, CircularProgress} from '@mui/material';
 
-export default function LabelBottomNavigation() {
-  const [value, setValue] = React.useState('recents');
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+const label = { inputProps: { 'aria-label': 'Switch demo' } };
 
+export default function BasicSwitches() {
   return (
-    <BottomNavigation sx={{ width: 500 }} value={value} onChange={handleChange}>
-      <BottomNavigationAction
-        label="Recents"
-        value="recents"
-        icon={<RestoreIcon />}
-      />
-      <BottomNavigationAction
-        label="Favorites"
-        value="favorites"
-        icon={<FavoriteIcon />}
-      />
-      <BottomNavigationAction
-        label="Nearby"
-        value="nearby"
-        icon={<LocationOnIcon />}
-      />
-      <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
-    </BottomNavigation>
+    <div>
+      
+      <Switch {...label} defaultChecked />
+      <Button variant="contained">Contained</Button>
+      <CircularProgress color="success" />
+
+    </div>
   );
 }
-
