33a33c8992818bcbb6b2add1ecd0106d615c94c3
sevinc995 <sevincseferova25@gmail.com>
2025-09-18 08:48:22 +0000

    change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import * as React from 'react';
-import {Switch, Button, CircularProgress} from '@mui/material';
+import {Rating, Switch, Button, CircularProgress} from '@mui/material';
 
 
 const label = { inputProps: { 'aria-label': 'Switch demo' } };
@@ -8,11 +8,11 @@ const label = { inputProps: { 'aria-label': 'Switch demo' } };
 export default function BasicSwitches() {
   return (
     <div>
-      
+      <Rating name="size-large" defaultValue={2} size="large" />
       <Switch {...label} defaultChecked />
       <Button variant="contained">Contained</Button>
       <CircularProgress color="success" />
-
+      
     </div>
   );
 }
